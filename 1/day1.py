2f3c44e0228f7241d981fc76d58df14f9ddf2f31
Steven Fitzpatrick <stevthedev@gmail.com>
2018-12-02 02:06:07 -0600

    Day 1

---
+++ 1/day1.py
@@ -0,0 +1,31 @@
+import os, csv
+
+# Read Input from File
+adjustments = []
+with open(os.path.join(os.getcwd(),'input')) as file:
+    data = csv.reader(file)
+    for row in data:
+        adjustments.append(int(row[0]))
+
+# 1: End Frequency
+freq = 0
+for number in adjustments:
+    freq += number
+print(freq) # Answer 1
+
+# 2: First Frequency Seen Twice
+freq = 0
+seen = []
+pairs = []
+
+while not pairs:
+    for number in adjustments:
+        freq += number
+        if freq in seen:
+            if freq not in pairs:
+                pairs.append(freq)
+                break
+        else:
+            seen.append(freq)
+
+print(pairs[0]) # Answer 2
